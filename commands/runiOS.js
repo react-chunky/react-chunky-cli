1ebda319b8320b3659e79724c8e61ab53a6dce3c
I. Dan Calinescu <idancalinescu@gmail.com>
2017-06-07 16:11:11 +0300

    aded ios bundling option

--- commands/runiOS.js
+++ commands/runiOS.js
@@ -1,6 +1,18 @@
 const coreutils = require('coreutils')
 
 function parseCommand(command) {
+    if (command.options.release) {
+        // Let's run it in release mode
+        coreutils.logger.info("Bundling the iOS app")
+        coreutils.run.reactNative(['bundle', 
+        '--platform', 'ios', 
+        '--dev', 'false', 
+        '--assets-dest', './ios/',
+        '--entry-file', 'node_modules/react-native-chunky/app/index.ios.js', 
+        '--bundle-output', 'ios/main.jsbundle'])
+        return
+    }
+
     coreutils.logger.info("Starting the iOS app")
     coreutils.run.reactNativeRun("ios")
 }
