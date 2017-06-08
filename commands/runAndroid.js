29b03890f370f7e818a9d8f267b2f1cbb7120be2
I. Dan Calinescu <idancalinescu@gmail.com>
2017-06-08 07:27:22 +0300

    added support for full android release installation

--- commands/runAndroid.js
+++ commands/runAndroid.js
@@ -10,9 +10,7 @@ function parseCommand(command) {
             '--entry-file', 'node_modules/react-native-chunky/app/index.android.js', 
             '--assets-dest', 'android/app/src/main/res/',
             '--bundle-output', 'android/app/src/main/assets/index.android.bundle']).
-        then(() =>  coreutils.run.async('cd', ['android'])).
-        then(() =>  coreutils.run.async('./gradlew', ['assembleRelease'])).
-        then(() =>  coreutils.run.async('./gradlew', ['installRelease']))
+        then(() =>  coreutils.run.async('cd', ['android', '&&', './gradlew', 'assembleRelease', '&&', './gradelw', 'installRelease']))
         return
     }
     
