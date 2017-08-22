bab83697a26ba7aebfc12bdda10a8bd0104b959e
I. Dan Calinescu <idancalinescu@gmail.com>
2017-08-22 15:47:41 +0300

    added android packager

--- executors/package/android.js
+++ executors/package/android.js
@@ -4,10 +4,12 @@ module.exports = function(optimize) {
     coreutils.logger.info(`Packaging the Android app ...`)
 
     return coreutils.run.reactNative(['bundle',
-          '--platform', 'android',
-          '--dev', 'false',
-          '--entry-file', 'node_modules/react-native-chunky/app/index.android.js',
-          '--assets-dest', 'android/app/src/main/res/',
-          '--bundle-output', 'android/app/src/main/assets/index.android.bundle']).
-      then(() =>  coreutils.run.async('cd', ['android', '&&', './gradlew', 'assembleRelease', '&&', './gradlew', 'installRelease']))
+              '--platform', 'android',
+              '--dev', 'false',
+              '--entry-file', 'node_modules/react-native-chunky/app/index.android.js',
+              '--assets-dest', 'android/app/src/main/res/',
+              '--bundle-output', 'android/app/src/main/assets/index.android.bundle']).
+          then(() =>  process.chdir('android')).
+          then(() =>  coreutils.run.async('./gradlew', ['assembleRelease'])).
+          then(() =>  coreutils.run.async('./gradlew', ['installRelease']))
 }
